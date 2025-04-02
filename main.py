58d48f945d5ec970a89a2b8d52b46d2bd8d01746
Franklin Sotolongo <franklinsotolongo@Franklins-Mini.lan>
2025-04-01 20:25:04 -0400

    Add rules functinality
    Add emojis to password rules display to make it fun
    
    Add emojis to password rules display to make it fun

---
+++ main.py
@@ -0,0 +1,11 @@
+# Define the rules for the password Validation
+Rules = [
+ "✨ Contains 8-16 characters",
+ "🔒 Contains one of the special characters: %$#^&*!@()",
+ "🔢 Contains at least one number 0-9",
+ "🔠 Contains at least one capital letter",
+ "🔤 Starts with a lowercase letter",
+ "🚫 Does not contain the phrase “pass” in any format",
+ "🚫 Does not contain the phrase “qwerty” in any format",
+ "🚫 Does not contain the phrase “123”",
+]
