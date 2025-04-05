228791bbf1702868e873d5170afc3bdfd4a19cf7
Franklinsotolongo <franklinsotolongo@gmail.com>
2025-04-05 12:24:25 -0400

    Added basic password logic to user interface (#3)
    Co-authored-by: Franklin Sotolongo <franklinsotolongo@Franklins-Mini.lan>

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 
+# The Mini project was Devided into two Parts: Maria is working on the passwrord validation logic and I am working on the user interface.
+# The code below is the user interface for the password validation project.
+# The code is designed to prompt the user for a password and check if it meets certain criteria.
+
 # Define a list called 'rules' that holds each password requirement, this was provided on the assigment documentation.
 # Each rule is represented as a string and we used emojis to make it fun and easy to understand.
 rules = [
@@ -23,3 +27,34 @@ def show_rules():
 
 #This is what the user will see first before entering the password.
 show_rules()
+
+#Ask the user to enter a password and store it in the cariable called "password"
+password = input("\nPlease enter a password that conforms to the above restrictions:\n\t\t")
+
+#Print the password back to the user so they can see what they entered.
+#This is for testing purposes while the logic is being developed. we will replace it with a validation funtion. 
+#validate_passord(password)
+print(f"Your password is: {password}")
+
+
+# This is a placegolder for the password validation function.
+# This function will be replace once the logic function is deloveloped and the validation function is defined.
+# For testing purposes, the function will pretend the password is always valid
+
+def validate_password(password):
+    return []
+
+#Use this validation fuction to check if the password meets the requirements.
+violations = validate_password(password)
+
+#Password Validation Logic will be  added here
+    
+#This is to show feedback to the user if the password is valid or not
+
+if violations:
+    print("\nThis password does not meet the following requirements:")
+    for rule in violations:
+        print(f'-{rule}')
+else:
+    print("\n🎉Success! Your Password meets all the requirements.")
+
